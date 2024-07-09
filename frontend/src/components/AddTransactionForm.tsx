0ffc3780cb7917cfda2af910445eefe128aacce6
Noam Anisfeld <NoamAnisfeld@gmail.com>
2024-07-10 02:07:46 +0300

    replace dropdowns on AddTranscationForm with a button-based multistep form

--- frontend/src/components/AddTransactionForm.tsx
+++ frontend/src/components/AddTransactionForm.tsx
@@ -1,27 +1,37 @@
-import { Button } from "@/components/ui/button"
+import { Button as UiButton, type ButtonProps as UiButtonProps } from "@/components/ui/button"
 import {
     Card,
     CardContent,
     CardFooter,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { useState, type FormEventHandler } from "react"
 import { type TransactionType } from '@/schemas';
-import CategorySelect from "./CategorySelect"
 import { useFetchCategories } from "./hooks/queries"
 import { addTransaction, fetchSubcategories } from "@/requests"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { Calendar } from "./ui/calendar"
+import { cn } from "@/tailwind/utils";
+
+function Button({ children, className, ...props }: UiButtonProps) {
+    return (
+        <UiButton
+            {...props}
+            className={cn("w-20 h-20 p-4 text-wrap", className)}
+        >
+            {children}
+        </UiButton>
+    )
+}
 
 export default function AddTransactionForm({ reset }: { reset: () => void }) {
 
-    const [transactionType, setTransactionType] = useState<TransactionType>('income');
-    const [amount, setAmount] = useState(0);
+    const [transactionType, setTransactionType] = useState<TransactionType | undefined>();
     const [categoryId, setCategoryId] = useState('');
     const [subcategoryId, setSubcategoryId] = useState('');
+    const [amount, setAmount] = useState(0);
     const [date, setDate] = useState(new Date());
-    const isDataValid = Boolean(amount && categoryId && subcategoryId);
+    const isDataValid = Boolean(transactionType && amount && categoryId && subcategoryId);
 
     const queryClient = useQueryClient();
     const { data: categories } = useFetchCategories();
@@ -53,64 +63,163 @@ export default function AddTransactionForm({ reset }: { reset: () => void }) {
 
     return (
         <form onSubmit={handleSubmit}>
-            <Card className="w-full mx-auto">
+            <Card className="w-full">
                 <CardContent className="grid p-4 gap-4">
-                    <div className="flex gap-4">
-                        <div className="flex flex-col gap-4">
-                            <div className="flex items-center gap-4">
-                                <span>{"הוספת "}</span>
-                                <select
-                                    autoFocus
-                                    value={transactionType}
-                                    // @ts-expect-error - to validate the type
-                                    onChange={e => setTransactionType(e.target.value)}
-                                >
-                                    <option value="income">הכנסה</option>
-                                    <option value="expense">הוצאה</option>
-                                </select>
-                                <Label>בסך</Label>
-                                <Input
-                                    id="amount"
-                                    type="number"
-                                    min={0}
-                                    max={1000000000}
-                                    required
-                                    value={amount}
-                                    onChange={e => setAmount(Number(e.target.value))}
-                                />
-                                ש"ח
-                            </div>
-                            <div>
-                                <CategorySelect
-                                    elementId="category"
-                                    label="קטגוריה"
-                                    categories={categories}
-                                    onSelected={setCategoryId}
-                                />
-                            </div>
-                            <div>
-                                <CategorySelect
-                                    elementId="subcategory"
-                                    label="תת קטגוריה"
-                                    categories={subcategories}
-                                    onSelected={setSubcategoryId}
-                                />
-                            </div>
-                        </div>
-                        <Calendar
-                            mode="single"
-                            required
-                            disabled={date => date > new Date()}
-                            selected={date}
-                            onSelect={date => date && setDate(date)}
-                            className="rounded-md border"
-                        />
-                    </div>
+                    <AddIncomeOrExpense
+                        {...{
+                            transactionType,
+                            setTransactionType,
+                            categories,
+                            subcategories,
+                            categoryId,
+                            setCategoryId,
+                            subcategoryId,
+                            setSubcategoryId,
+                            amount,
+                            setAmount,
+                            date,
+                            setDate,
+                        }}
+                    />
                 </CardContent>
                 <CardFooter>
                     <Button className="w-full" disabled={!isDataValid}>הוספה</Button>
                 </CardFooter>
             </Card>
         </form>
+    )
+}
+
+function AddIncomeOrExpense({
+    transactionType,
+    setTransactionType,
+    categories,
+    subcategories,
+    categoryId,
+    setCategoryId,
+    subcategoryId,
+    setSubcategoryId,
+    amount,
+    setAmount,
+    date,
+    setDate,
+}) {
+    if (transactionType) {
+        return (
+            <SelectCategory
+                {...{
+                    transactionType,
+                    categories: categories.filter(category => category.type === transactionType),
+                    subcategories,
+                    categoryId,
+                    setCategoryId,
+                    subcategoryId,
+                    setSubcategoryId,
+                    amount,
+                    setAmount,
+                    date,
+                    setDate,
+                }}
+            />
+        )
+    }
+
+    return (
+        <div className="flex justify-center gap-4">
+            <Button onClick={() => setTransactionType('income')}>הוספת הכנסה</Button>
+            <Button onClick={() => setTransactionType('expense')}>הוספת הוצאה</Button>
+        </div>
+    )
+}
+
+function SelectCategory({
+    categories,
+    subcategories,
+    categoryId,
+    setCategoryId,
+    subcategoryId,
+    setSubcategoryId,
+    amount,
+    setAmount,
+    date,
+    setDate,
+}) {
+    if (categoryId) {
+        return (
+            <SelectSubcategory
+                {...{
+                    subcategories,
+                    subcategoryId,
+                    setSubcategoryId,
+                    amount,
+                    setAmount,
+                    date,
+                    setDate,
+                }}
+            />
+        )
+    }
+
+    return (<div className="flex gap-4">
+        {categories.map(category => (
+            <Button
+                key={category._id}
+                onClick={() => setCategoryId(category._id)}
+            >
+                {category.name}
+            </Button>
+        ))}
+    </div>)
+}
+
+function SelectSubcategory({
+    subcategories,
+    subcategoryId,
+    setSubcategoryId,
+    amount,
+    setAmount,
+    date,
+    setDate,
+}) {
+    if (subcategoryId) {
+        return (
+            <SelectAmountAndDate
+                {...{
+                    amount,
+                    setAmount,
+                    date,
+                    setDate,
+                }}
+            />
+        )
+    }
+
+    return (<div className="flex gap-4">
+        {subcategories.map(subcategory => (
+            <Button
+                key={subcategory._id}
+                onClick={() => setSubcategoryId(subcategory._id)}
+            >
+                {subcategory.name}
+            </Button>
+        ))}
+    </div>)
+}
+
+function SelectAmountAndDate({ amount, setAmount, date, setDate }) {
+    return (
+        <div className="flex gap-4">
+            <Input
+                type="number"
+                value={amount}
+                onChange={e => setAmount(Number(e.target.value))}
+                placeholder="סכום"
+            />
+            <Input
+                type="date"
+                value={date.toISOString().split('T')[0]}
+                onChange={e => setDate(new Date(e.target.value))}
+            />
+        </div>
     )
 }
